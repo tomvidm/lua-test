f2ebf31a0901e856804856caa1127ab05b5c1ddc
Tom Vidar Michalsen <tomvidm@gmail.com>
2017-10-06 14:43:44 +0200

    Work on build script

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 * CMake
 ## How to build
 ### Linux
+Arguments to pass to build.sh:
+* clean - removes contents from /build/
 Make sure prerequisites are installed using magic apt-get commands and run `/build.sh`
 ### Windows
 SOMEHOW
--- build.sh
+++ build.sh
@@ -1,10 +1,36 @@
-cd bin
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p build
+
+
+
+
+# Go in there and build that shit
+cd build
+
+if [ $1 == "clean" ]
+then
+	echo "*** Cleaning up /build/"
+	rm -rf *
+fi
+
 echo "*** Running cmake .."
 cmake ..
-echo "*** Running make"
-make
-cd src
-echo "*** Build complete."
-echo "*** Running luatest"
-./luatest
-cd ../..
+
+if [ "$(expr substr $(uname) 1 5)" == "Linux" ]
+then
+	echo "*** It seems you are running Linux. Everything will be prepared for you my master."
+	echo "*** Running make"
+	make
+	cd src
+	echo "*** Build complete."
+	echo "*** Running luatest"
+	./luatest
+	cd ../..
+else
+	echo "*** Not Linux!? Ye will not be rewarded with automated compilation and running..."
+	echo "*** A Visual Studio solution should be in /build/"
+	cd ..
+fi
