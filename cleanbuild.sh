1197744ddd74d7c70a44f1daa54c9281ded20786
Tom Vidar Michalsen <tomvidm@gmail.com>
2017-10-06 14:12:09 +0200

    Updated cleanbuild.sh to hopefully detect linux or not

--- cleanbuild.sh
+++ cleanbuild.sh
@@ -1,12 +1,30 @@
+#!/usr/bin/env bash
+
+if [ ! -d "/bin" ]
+then
+    echo "*** Creating directory /bin"
+    mkdir bin
+fi
+
+
+
 cd bin
-echo "*** Removing bin/"
+echo "*** Cleaning up /bin/"
 rm -rf *
 echo "*** Running cmake .."
 cmake ..
-echo "*** Running make"
-make
-cd src
-echo "*** Build complete."
-echo "*** Running luatest"
-./luatest
-cd ../..
+
+
+if [ "$(expr substr $(uname) 1 5)" == "Linux" ]
+then
+	echo "*** Running make"
+	make
+	cd src
+	echo "*** Build complete."
+	echo "*** Running luatest"
+	./luatest
+	cd ../..
+else
+	echo "*** Not Linux!? Ye will not be rewarded with automated compilation and running..."
+	cd ..
+fi
